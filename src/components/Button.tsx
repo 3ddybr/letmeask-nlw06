ccb9afce4677de9a7a035b6998086d8b30298a87
Alberto Consoli de Araujo <68253941+3ddybr@users.noreply.github.com>
2022-04-28 21:58:05 -0300

    projeto concluido

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import {ButtonHTMLAttributes} from 'react';
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    IsOutlined?: boolean
+}
+
+export function Button ({IsOutlined = false, ...props}: ButtonProps) {
+    return (
+        <button 
+            className={`button ${IsOutlined? 'outlined' : '' }`} 
+            {...props}
+        />
+    )
+}
